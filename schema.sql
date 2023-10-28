ce8214908ae58d54c2c7fba29999e79153c929ca
Elis Memic <elismemic5@gmail.com>
2023-10-28 18:01:25 +0200

    Add tables vets, visits and specialization

--- schema.sql
+++ schema.sql
@@ -31,6 +31,9 @@ ALTER TABLE animals DROP COLUMN id;
 
 ALTER TABLE animals RENAME new_id TO id ;
 
+ALTER TABLE animals
+ADD PRIMARY KEY (id);
+
 -- Add the 'species_id' column as a foreign key referencing the 'species' table
 ALTER TABLE animals
 ADD COLUMN species_id INTEGER REFERENCES species(id);
@@ -39,4 +42,23 @@ ADD COLUMN species_id INTEGER REFERENCES species(id);
 ALTER TABLE animals
 ADD COLUMN owner_id INTEGER REFERENCES owners(id);
 
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    date_of_graduation DATE
+);
 
+CREATE TABLE specializations (
+    vet_id INTEGER,
+    species_id INTEGER,
+    PRIMARY KEY (vet_id, species_id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id),
+    FOREIGN KEY (species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    visit_date DATE NOT NULL
+);
